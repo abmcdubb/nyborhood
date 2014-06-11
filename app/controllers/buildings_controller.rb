5fc5a76df7a0166ab28580fe774a110f4c9ab9b2
abmcdubb <abmcwilliams@gmail.com>
2014-06-11 11:39:28 -0400

    update to search function

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -5,10 +5,14 @@ include BuildingsHelper
     if params.include?("building") && !params["building"]["address"].blank?
       address = standardize_address_input(params["building"]["address"])  
       @building = Building.search_by_address(address) if params.include?("building")
-      @complaint_count = @building.complaints.count
-      @top_5_complaints = Complaint.complaints_by_building(@building.id)
-
-      @gmail_query = @building.address.gsub(' ', '+') + "+#{@building.zip}"
+        if @building
+          @complaint_count = @building.complaints.count
+          @top_5_complaints = Complaint.complaints_by_building(@building.id)
+          @gmail_query = @building.address.gsub(' ', '+') + "+#{@building.zip}"
+          flash[:notice] = nil
+        else
+          flash[:notice] = "No buildings in our database matched that search. Please include only house number, street name, and street type. For example: '123 Fake Street'"
+        end
     end
   end
 end
--- app/helpers/buildings_helper.rb
+++ app/helpers/buildings_helper.rb
@@ -3,14 +3,6 @@ module BuildingsHelper
 ADDRESS_REPLACEMENTS = {
                         'E' => 'EAST',
                         'W' => 'WEST',                 
-                        'ST' => 'STREET',
-                        'AVE' => 'AVENUE',
-                        'BLVD' => 'BOULEVARD',
-                        'PL' => 'PLACE',
-                        'PKWY' => 'PARKWAY',
-                        'RD' => 'ROAD',
-                        'DR' => 'DRIVE',
-                        'CT' => 'COURT',
                         'FIRST' => '1',
                         'SECOND' => '2',
                         'THIRD' => '3',
@@ -32,6 +24,16 @@ ADDRESS_REPLACEMENTS = {
                         'NINETEENTH' => '19',
                         'TWENTIETH' => '20'
                         }
+STREET_NAME_REPLACEMENTS = {
+                'ST' => 'STREET',
+                'AVE' => 'AVENUE',
+                'BLVD' => 'BOULEVARD',
+                'PL' => 'PLACE',
+                'PKWY' => 'PARKWAY',
+                'RD' => 'ROAD',
+                'DR' => 'DRIVE',
+                'CT' => 'COURT',
+                }
 
 REGEX_FOR_NUMBERS = /\dND|\dST|\dRD|\dTH/
 
@@ -42,6 +44,7 @@ REGEX_FOR_NUMBERS = /\dND|\dST|\dRD|\dTH/
       word.gsub!(word, ADDRESS_REPLACEMENTS) if ADDRESS_REPLACEMENTS[word]
       word.gsub!(word.match(REGEX_FOR_NUMBERS)[0], word.match(REGEX_FOR_NUMBERS)[0][0]) if word.match(REGEX_FOR_NUMBERS)
     end
+    address[-1].gsub!(address[-1], STREET_NAME_REPLACEMENTS) if STREET_NAME_REPLACEMENTS[address[-1]]
     address = address.join(' ')
   end
 
